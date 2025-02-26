cb1984a03d4df81d267135523e1ff5bedd8743d8
LBwaks <obwakuvictor@gmail.com>
2025-02-26 06:45:55 +0300

    Faker Install

--- backend/Intaman/settings.py
+++ backend/Intaman/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 import os
 from datetime import timedelta
 from django.conf import settings
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,6 +94,10 @@ WSGI_APPLICATION = 'Intaman.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASE_URL=os.getenv("DATABASE_URL")
+# DATABASES ={"default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1200),}
+
 
 DATABASES = {
     'default': {
---
+++ backend/Task/factory.py
@@ -0,0 +1,82 @@
+import factory
+from .models import  Sector, Source, Support, Status, Issue, Priority, Task, TaskHistory, TaskFiles
+from django.contrib.auth.models import User
+import uuid,datetime
+# from factory.Faker import Faker
+# fake=Faker()
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+    username=factory.Faker('user_name')
+    email = factory.LazyAttribute(lambda o: '%s@example.com' % o.username)
+    password=  factory.PostGenerationMethodCall('set_password','password123')
+
+
+class SectorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Sector
+
+    sector_name = factory.Faker("sentence",nb_words=2)
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker('sentence',nb_words=10)
+    is_featured = False
+    is_published = False
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
+class SourceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model =Source
+
+    source_name = factory.Faker("random_element",elements=['Customer CAre-2','Technian-Field','WEbsite-0'])
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker("paragraph")
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
+
+class SupportFactory(factory.django.DjangoModelFactory):
+    class Meta :
+        model =Support
+    support_name = factory.Faker("random_element",elements=['Fiber-Cut','Los2','Slow-0','BLink','Test'])
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker("paragraph")
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
+
+class IssueFactory(factory.django.DjangoModelFactory):
+    class Meta :
+        model =Issue
+    issue_type = factory.Faker('word')
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker("paragraph")
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
+
+class StatusFactory(factory.django.DjangoModelFactory):
+    class Meta :
+        model =Status
+    status_name = factory.Faker('word')#factory.Faker("random_element",elements=['New','Working On it','REsolved','Closed','Stop'])
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker("paragraph")
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
+class PriorityFactory(factory.django.DjangoModelFactory):
+    class Meta :
+        model =Priority
+    priority_type = factory.Faker('word')#factory.Faker("random_element",elements=['Critiacl','Major','Blocker','Low','Medium'])
+    slug = factory.LazyFunction(uuid.uuid4)
+    user = factory.SubFactory(UserFactory)
+    description = factory.Faker("paragraph")
+    create_date = factory.LazyFunction(datetime.datetime.now)
+    update_date = factory.LazyFunction(datetime.datetime.now)
+
