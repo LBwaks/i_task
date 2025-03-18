cb87a63b883785509f257bdfbc64689fa976d365
LBwaks <obwakuvictor@gmail.com>
2025-03-18 06:27:23 +0300

    change_status

--- backend/Task/views.py
+++ backend/Task/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from rest_framework import generics,viewsets
+from rest_framework import generics,viewsets,status
 from .serializers import TaskSerializer,SectorSerializer,SourceSerializer,IssueSerializer,SupportSerializer,IssueSerializer,SupportSerializer,StatusSerializer,PrioritySerializer
 from .models import Task, Sector,Source,Issue,Support,Status,Priority
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 # from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -18,6 +20,25 @@ class TaskViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         return super().perform_create(serializer)
+    
+    @action(detail=True,methods=['put'],name='change_assignee')
+    def change_asignee(self,request,pk=None):
+        task = self.get_object()
+        serializer = A;
+
+    @action(detail=True, methods=['put'],name='change_status')
+    def change_status(self,request,pk=None):
+        status_obj=self.get_object()
+        serializer=TaskSerializer(status_obj,data=request.data,partial=True,context={"request":request})
+        
+        if serializer.is_valid():
+            #status_obj.change_status(serializer.validated_data["status"])
+            status_obj.save()
+            return Response({'status':'Status Updated'})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 class SectorViewSet(viewsets.ModelViewSet):
